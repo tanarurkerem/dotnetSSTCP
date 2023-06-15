54af0c8f06e72231836b7f241eb753adfec7909b
Palocz Istvan <istvan@palocz.hu>
2023-06-15 10:13:49 +0200

    Add app.json and environment variables

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,14 +1,22 @@
 ﻿using SuperSimpleTcp;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string serverName = System.Environment.GetEnvironmentVariable("SERVER_NAME") ?? "127.0.0.1";
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("/app/Client/app.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        string serverName = config["serverName"] ?? "127.0.0.1";
+        string serverPort = config["serverPort"] ?? "9000";
         Console.WriteLine($"Server name: {serverName}");
+        Console.WriteLine($"Server Port: {serverPort}");
         // instantiate
-        SimpleTcpClient client = new SimpleTcpClient($"{serverName}:9000");
+        SimpleTcpClient client = new SimpleTcpClient($"{serverName}:{serverPort}");
 
         // set events
         client.Events.Connected += Connected;
